2fbbea956b32274cf575b7b1b0c75d2ffcb17ac2
sudarshan, Manoj <manoj.sudarshans@honeywell.com>
2019-09-29 09:42:38 +0530

    added assests and Toolbar

--- app/src/main/java/com/example/githubtrends/MainActivity.kt
+++ app/src/main/java/com/example/githubtrends/MainActivity.kt
@@ -2,18 +2,20 @@ package com.example.githubtrends
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.githubtrends.ui.main.MainFragment
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        val mTitle = toolbar.findViewById(R.id.toolbar_title) as TextView
+
+        setSupportActionBar(toolbar)
+        mTitle.setText("TRENDING")
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
     }
 
 }
